2c2906cdec76390dc750a3473cfa51c9a15f8766
Eideann <aingeru.scotland@gmail.com>
2022-04-27 22:19:25 +0200

    Plugin: Nvim-tree

--- init.lua
+++ init.lua
@@ -14,8 +14,10 @@ vim.call("plug#begin")
     Plug "itchyny/lightline.vim"
     Plug("nvim-treesitter/nvim-treesitter", {["do"] = vim.fn[":TSUpdate"]})    
     Plug 'tomasiser/vim-code-dark'
+    Plug 'kyazdani42/nvim-web-devicons' 
+    Plug 'kyazdani42/nvim-tree.lua'
 vim.call("plug#end")
 
 -- Plugins are initialised, now we can append our config.
 require "plugin_treesitter"
-
+require "plugin_nvim-tree"
